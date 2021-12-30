26ac8009cd92c07bc155f7af174b2b39cf1f5943
RianNugroho <Riannugroho4@gmail.com>
2021-12-30 21:14:56 +0900

    fix bug

--- main.py
+++ main.py
@@ -9,10 +9,9 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://en.wikipedia.org/wiki/SpaceX")
 
+wikipedia_xpath="/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[9]/td/div/ul/li[1]/a[1]"
 
-wikipedia_ceo_xpath="/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[9]/td/div/ul/li[1]/a[1]"
-
-wikipedia_spacexCEO=driver.find_element("xpath",wikipedia_ceo_xpath).text
+wikipedia_spacexCEO=driver.find_element("xpath",wikipedia_xpath).text
 
 driver.quit()
 
@@ -25,10 +24,7 @@ query="""query{
 response=requests.post("https://api.spacex.land/graphql/", json={'query':query})
 print(response.status_code)
 api_spacexCEO=response.json()
-print("CEO SpaceX menurut Wikipedia",wikipedia_ceo_xpath)
+print("CEO SpaceX menurut Wikipedia",wikipedia_spacexCEO)
 print('CEO SpaceX menurut api.spacex.land/graphql',api_spacexCEO)
 print("Apakah CEO SpaceX pada wikipedia sama dengan api.spacex.land ?")
 print(wikipedia_spacexCEO==api_spacexCEO["data"]["company"]["ceo"])
-
-
-requests.
