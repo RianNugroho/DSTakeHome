d7ef9f6ff38aa74611e5aabc500cb25982d2f955
RianNugroho <Riannugroho4@gmail.com>
2021-12-30 20:35:08 +0900

    Datasaur Takehome work

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+import requests
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+driver.get("https://en.wikipedia.org/wiki/SpaceX")
+
+
+wikipedia_ceo_xpath="/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[9]/td/div/ul/li[1]/a[1]"
+
+wikipedia_spacexCEO=driver.find_element("xpath",wikipedia_ceo_xpath).text
+
+driver.quit()
+
+query="""query{
+  company {
+    ceo
+  }
+}
+"""
+response=requests.post("https://api.spacex.land/graphql/", json={'query':query})
+print(response.status_code)
+api_spacexCEO=response.json()
+print("CEO SpaceX menurut Wikipedia",wikipedia_ceo_xpath)
+print('CEO SpaceX menurut api.spacex.land/graphql',api_spacexCEO)
+print("Apakah CEO SpaceX pada wikipedia sama dengan api.spacex.land ?")
+print(wikipedia_spacexCEO==api_spacexCEO["data"]["company"]["ceo"])
+
+
