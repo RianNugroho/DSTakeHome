5b2d57aa193cc6b5021eaa58de8e2b1fa093c15b
RianNugroho <Riannugroho4@gmail.com>
2021-12-30 21:16:12 +0900

    update code

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 driver.get("https://en.wikipedia.org/wiki/SpaceX")
 
 wikipedia_xpath="/html/body/div[3]/div[3]/div[5]/div[1]/table[1]/tbody/tr[9]/td/div/ul/li[1]/a[1]"
-
 wikipedia_spacexCEO=driver.find_element("xpath",wikipedia_xpath).text
-
 driver.quit()
 
 query="""query{
